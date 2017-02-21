0fbc0b2c6241fa9f62e8d165d584642698777159
wilferaciolli <wiliam334@hotmail.com>
2017-02-21 22:21:03 +0000

    Manual Testing - Category 16 - This lesson was settng up MySQL driver on standalone.xml.

--- library-model/src/main/resources/db.sql
+++ library-model/src/main/resources/db.sql
@@ -1,4 +1,12 @@
-create table lib_category (
-	id      bigserial        not null primary key,
-	name    varchar(25)      not null unique
+
+--PostGre
+CREATE TABLE lib_category (
+	id      bigserial        NOT NULL PRIMARY KEY,
+	name    varchar(25)      NOT NULL UNIQUE
+);
+
+--MySQL
+CREATE TABLE lib_category (
+	id      bigint           NOT NULL PRIMARY KEY AUTO_INCREMENT DEFAULT NULL,
+	name    varchar(25)      NOT NULL UNIQUE
 );
