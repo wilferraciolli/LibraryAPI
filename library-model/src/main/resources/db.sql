1130d118562cb94a02f172fd8d4637c4325d1990
wilferaciolli <wiliam334@hotmail.com>
2017-09-06 23:22:20 +0100

    Manual testing - Author 8 - Manual testing get author end points and filters

--- library-model/src/main/resources/db.sql
+++ library-model/src/main/resources/db.sql
@@ -10,3 +10,10 @@ CREATE TABLE lib_category (
 	id      bigint           NOT NULL PRIMARY KEY AUTO_INCREMENT DEFAULT NULL,
 	name    varchar(25)      NOT NULL UNIQUE
 );
+
+CREATE TABLE lib_author (
+	id       bigint         NOT NULL PRIMARY KEY AUTO_INCREMENT DEFAULT NULL,
+	name     varchar(40)     NOT NULL
+);
+--Create an index on the author table for the name column because it is used for search
+create index idx_author_name on lib_author(name);
